052b0230393e486b28744f4f76a7db2b05f3c228
Combos93 <combos93@mail.ru>
2019-06-20 20:25:35 +0600

    restyle views

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -25,12 +25,12 @@ class TodoItemsController < ApplicationController
 
   private
 
-  def set_todo_item
-    @todo_item = @todo_list.todo_items.find(params[:id])
-  end
-
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   def todo_item_params
